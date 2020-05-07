5452f2c0780ca4456fb6678e079a28a7b3dc78ad
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-07 19:47:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,7 +51,7 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|
+  array.collect.with_index do |element, index|
     if index == 0 || index > 1 
       element << "s"
     else 
